f1d4d6d5777091ac46306ff93c92c188a718a0dc
code-iddih <idinyamwaya@gmail.com>
2024-06-25 01:17:32 +0300

    Completing Tasks

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  let result;
+
+  if (feet >= 2500) {
+    result = "No can do.";
+
+  } else if (feet >=2000) {
+    result = "I will gladly take your thirty bucks.";
+
+  } else if (feet >= 400) {
+    result = "That will be twenty bucks.";
+
+  } else {
+    result = "This one is on me!";
+  }
+
+  return result;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  const cityDes = city === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return cityDes;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  let tipReponse;
+
+  switch (tip) {
+    case "generous":
+      tipReponse = "Thank you so much.";
+      break;
+
+    case "not as generous":
+      tipReponse = "Thank you.";
+      break;
+
+    default:
+      tipReponse = "Bye.";
+      break;
+  }
+
+  return tipReponse;
+
 }
